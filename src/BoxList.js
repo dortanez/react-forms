9673260ecf2a9fd0b9083a22dc31c97297bdd565
Danisa Ortanez <dortanez@yahoo.com>
2020-12-30 10:29:07 -0800

    react forms

---
+++ src/BoxList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from './Box';
+import NewBoxForm from './NewBoxForm';
+
+const BoxList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BoxList;
---
+++ src/NewBoxForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewBoxForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NewBoxForm;
